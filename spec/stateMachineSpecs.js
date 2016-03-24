db97cad189db0a0857de6186b7ebe24322f8d256
Dave Timmerman <dave@frontmen.nl>
2016-03-24 16:14:15 +0100

    (update) added test

--- spec/stateMachineSpecs.js
+++ spec/stateMachineSpecs.js
@@ -130,6 +130,21 @@
     q.equal(_in2, {some: 'data'});
   });
 
+  test('eventName is added to enter method parameters', function () {
+    var _in;
+    newStates.build()
+      .state('simple', {
+        enter: function (str, data, name) {
+          _in = name;
+        }
+      })
+    .event('withParams', 'young', 'simple')
+    ;
+
+    newStates.withEventName('a string', {some: 'data'});
+    q.equal(_in, 'simple');
+  });
+
   test('does not overwrite existing properties', function () {
     var st = states(function () {
       this.state('simple');
